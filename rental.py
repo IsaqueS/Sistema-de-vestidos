a5d0fb883dd58a8d8f89c51022ec77e812989a39
Isaque Sebold de Almeida <38296655+IsaqueS@users.noreply.github.com>
2024-09-30 11:01:34 -0300

    Added "bones" for the rental class

---
+++ rental.py
@@ -0,0 +1,8 @@
+#CLASSE DE AGENDA SERA REFEITA, PARA ATENDER MELHOR OS REQUISITOS
+
+#Ainda não temos uma forma boa de organizar os dados por data
+
+class Rental:
+
+    def __init__(self) -> None:
+        pass
