fa91a4f6a691f648b743576aeb07b5e6badb146d
Thiago Soares Prado Carneiro <thiagopcarneiro@gmail.com>
2024-10-21 19:59:00 -0300

    Add files via upload

---
+++ person.py
@@ -0,0 +1,35 @@
+class Person:
+    def __init__(self, name: str, number: int, email: str) -> None:
+        self.__name = None
+        if isinstance(name, str):
+            self.__name = name
+        self.__number = None
+        if isinstance(number, int):
+            self.__number = number
+        self.__email = None
+        if isinstance(email, str):
+            self.__email = email
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @name.setter
+    def name(self, name: str) -> None:
+        if isinstance(name, str):
+            self.__name = name
+    @property
+    def number(self) -> int:
+        return self.__number
+
+    @number.setter
+    def number(self, number: int):
+        if isinstance(number, int):
+            self.__number = number
+    @property
+    def email(self) -> str:
+        return self.__email
+
+    @email.setter
+    def email(self, email: str):
+        if isinstance(email, str):
+            self.__email = email
