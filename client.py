0b0f90e91a8ce4efdb615e3c710d5217ce406190
Thiago Soares Prado Carneiro <thiagopcarneiro@gmail.com>
2024-11-04 18:49:07 -0300

    Update client.py

--- client.py
+++ client.py
@@ -7,16 +7,6 @@ class Client(Person):
 
     def __init__(self, name: str, number: int, email: str, instagram: str, cpf: int, birth_date: datetime) -> None:
         super().__init__(name, number, email)
-        #self.__name = None
-        #if isinstance(name, str):
-        #    self.__name = name
-        #self.__number = None
-        #if isinstance(number, int):
-        #    self.__number = number
-        #self.__email = None
-        #if isinstance(email, str):
-        #    self.__email = email
-        #self.__instagram = None
         if isinstance(instagram, str):
             self.__instagram = instagram
         self.__cpf = None
