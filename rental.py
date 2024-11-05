cbd3131ceb660c6f1f0c594d7e7b230b9708f6bd
Thiago Soares Prado Carneiro <thiagopcarneiro@gmail.com>
2024-11-04 22:16:58 -0300

    Update rental.py

--- rental.py
+++ rental.py
@@ -2,6 +2,7 @@ import datetime
 from supplier import Supplier
 from suit import Suit
 from client import Client
+from random import randint
 
 #CLASSE DE AGENDA PODE SER REFEITA, PARA ATENDER MELHOR OS REQUISITOS
 
@@ -9,20 +10,26 @@ from client import Client
 
 class Rental:
 
-    def __init__(self, date: datetime, wait_time: datetime, products: list[Suit], client: Client) -> None:
+    def __init__(self, date: datetime, wait_time: datetime, products: list, client: Client) -> None:
+        self.__rent_code = randint(0, 2000000)
         self.__date = None
-        if isinstance(date, datetime):
-            self.__date = date
+        #if isinstance(date, datetime):
+            #self.__date = date
         self.__products = None
-        if isinstance(products, list[Suit]):
+        if isinstance(products, list):
             self.__products
         self.__wait_time = None
-        if isinstance(wait_time, datetime):
-            self.__wait_time = wait_time
+        #if isinstance(wait_time, datetime):
+            #self.__wait_time = wait_time
         self.__client = None
         if isinstance(client, Client):
             self.__client = client
-        
+
+    @property
+    def rent_code(self) -> int:
+        return self.__rent_code
+
+    @property
     def date(self) -> datetime:
         return self.__date
 
@@ -30,22 +37,26 @@ class Rental:
     def date(self, date: datetime) -> None:
         if isinstance(date,datetime):
             self.__date = date
-    
+
+    @property
     def wait_time(self) -> datetime:
         return self.__wait_time
     
     @wait_time.setter
     def wait_time(self, wait_time: datetime) -> None:
-        if isinstance(wait_time, datetime):
-            self.__wait_time = wait_time
-    
-    def products(self) -> list[Suit]:
+        #if isinstance(wait_time, datetime):
+        self.__wait_time = wait_time
+
+    @property
+    def products(self) -> list:
         return self.__products
+    @property
 
     def add_product(self, product: Suit) -> None:
         if isinstance(product,Suit):
             self.__products.append(product)
-    
+
+    @property
     def client(self) -> Client:
         return self.__client
     
